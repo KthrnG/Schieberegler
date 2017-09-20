4e7de2ba857f4fac21ac3ba54e5c6de1843d72b6
kthrnG <kgedrat@hfk-bremen.de>
2017-09-20 18:37:27 +0200

    Zweiter Testlauf Motor bewegen

--- Schieberegler.ino
+++ Schieberegler.ino
@@ -1,7 +1,12 @@
+int motorA = 5;
+int motorB = 6;
+int enablePin = 3;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode (A0, INPUT);
+  pinMode(enablePin, OUTPUT);
 }
 
 void loop() {
@@ -9,5 +14,12 @@ void loop() {
   int value = map(analogRead(A0), 0, 1023, 0, 100);
   Serial.println(value);
   delay(100);
+
+//  digitalWrite(enablePin, HIGH);
+//  analogWrite(motorA, 0);
+//  analogWrite(motorB, 255);
+//  delay(100);
+//  digitalWrite(enablePin, LOW);
+
 }
 
